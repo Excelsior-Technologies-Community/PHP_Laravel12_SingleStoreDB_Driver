c27447da73247ecb196ed40fcbd26e3978081672
i33excelsior <i3.3.excelsior@gmail.com>
2026-05-05 19:03:42 +0530

    validation , search , pagination, filter  functionality add

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,25 +8,50 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     // ✅ INDEX (GET all)
-    public function index()
-    {
-        return response()->json(Product::all());
+  public function index(Request $request)
+{
+    $query = Product::query();
+
+    // search
+    if ($request->search) {
+        $query->where('name', 'LIKE', "%{$request->search}%");
     }
+
+    // price filter
+    if ($request->min_price && $request->max_price) {
+        $query->whereBetween('price', [$request->min_price, $request->max_price]);
+    }
+
+    // sorting
+    if ($request->sort_by && $request->sort_order) {
+        $query->orderBy($request->sort_by, $request->sort_order);
+    }
+
+    // pagination
+    $products = $query->paginate($request->limit ?? 10);
+
+    return response()->json($products);
+}
 
     // ✅ STORE (POST)
     public function store(Request $request)
-    {
-        $product = Product::create([
-            'name' => $request->name,
-            'price' => $request->price,
-            'meta' => ['color' => 'red']
-        ]);
+{
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'price' => 'required|numeric|min:1',
+    ]);
 
-        return response()->json([
-            'message' => 'Product Created',
-            'data' => $product
-        ]);
-    }
+    $product = Product::create([
+        'name' => $request->name,
+        'price' => $request->price,
+        'meta' => ['color' => 'red']
+    ]);
+
+    return response()->json([
+        'message' => 'Product Created',
+        'data' => $product
+    ]);
+}
 
     // ✅ SHOW (GET single)
     public function show($id)
